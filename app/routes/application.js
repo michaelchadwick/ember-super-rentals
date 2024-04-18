b4dd0c50bfdb6c0d2cb92dd523b478b0a08acea5
Michael Chadwick <michael.chadwick@gmail.com>
2024-04-18 16:33:49 -0700

    <title> now dynamic per environment

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,12 +6,15 @@ export default class ApplicationRoute extends Route {
   @service headData;
 
   afterModel() {
+    this.headData.title = 'SuperRentals';
     this.headData.ogTitle = 'SuperRentals Ember Tutorial';
 
     if (ENV.environment == 'production') {
       this.headData.faviconType = 'prod';
+      this.headData.envTitle = 'prod';
     } else {
       this.headData.faviconType = 'dev';
+      this.headData.envTitle = 'dev';
     }
   }
 }
