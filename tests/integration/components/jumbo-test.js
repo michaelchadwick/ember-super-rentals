9ce692c618b44ea02166c4b18dd49b351ecc2de1
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-07 15:46:47 -0700

    added test for tomster popover

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'super-rentals/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | jumbo', function (hooks) {
@@ -12,5 +12,13 @@ module('Integration | Component | jumbo', function (hooks) {
     assert.dom('.jumbo').exists();
     assert.dom('.jumbo').hasText('Hello World');
     assert.dom('.jumbo .tomster').exists();
+  });
+
+  test('it renders an EmberPopover when tomster is clicked', async function (assert) {
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
+
+    await click('.jumbo .tomster');
+    assert.dom('.ember-popover').exists();
+    assert.dom('.ember-popover .ember-popover-inner div').hasText('Tomster says: You are alive!');
   });
 });
